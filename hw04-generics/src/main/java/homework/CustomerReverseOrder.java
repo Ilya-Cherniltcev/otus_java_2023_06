6a19c47e4ada973b396eb63555b01a47d114fde7
Ilya Cherniltcev <cherniltcev@mail.ru>
2023-07-25 11:43:10 +0500

    refactoring using Deque

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,30 +1,18 @@
 package homework;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    List<Customer> customers;
-    int counter;
-
-    public CustomerReverseOrder() {
-        customers = new ArrayList<>();
-        counter = 0;
-    }
+    Deque<Customer> customers = new ArrayDeque<>();
 
     public void add(Customer customer) {
         customers.add(customer);
-        counter += 1;
     }
 
-    public Customer take() {
-        if (counter > 0) {
-            counter -= 1;
-        }
-        return customers.get(counter);
+    public Customer take() throws NoSuchElementException {
+        return customers.removeLast();
     }
 }
